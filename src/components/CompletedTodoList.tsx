8621db338d79a42d9ecf19cd5b4768825f0100bc
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-03-12 17:55:01 +0530

    Update CompletedTodoList.tsx

--- src/components/CompletedTodoList.tsx
+++ src/components/CompletedTodoList.tsx
@@ -4,9 +4,13 @@ import styles from '../styles/main.module.css';
 import TodoType from "../models/todo_type";
 import TodoItem from "./TodoItem";
 
-const CompletedTodoList: React.FC<{ items: TodoType[], reEnterHandler: (id: string) => void }> = (props) => {
+const CompletedTodoList: React.FC<{ items: TodoType[], reEnterHandler: (id: string) => void, deleteTodoHandler: (id: string) => void }> = (props) => {
     const reEnterTodoHandler = (id: string) => {
         props.reEnterHandler(id)
+    }
+
+    const deleteHandler = (id: string) => {
+        props.deleteTodoHandler(id);
     }
 
     return (
@@ -14,7 +18,7 @@ const CompletedTodoList: React.FC<{ items: TodoType[], reEnterHandler: (id: stri
             {props.items.length === 0 && <h3>Yet to complete a To-Do</h3>}
             {props.items.length !== 0 && props.items
                 .map((todo) => 
-                    <TodoItem key={todo.id} todo={todo} changeToDoHandler={reEnterTodoHandler} checked={true}/>
+                    <TodoItem key={todo.id} todo={todo} changeToDoHandler={reEnterTodoHandler} checked={true} deleteToDoHandler={deleteHandler}/>
                 )
             }
         </ul>

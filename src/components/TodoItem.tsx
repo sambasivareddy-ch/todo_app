715675fbca7482e6c1d851c67d8be7c0ba34230b
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-03-12 17:55:23 +0530

    Update TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import TodoType from "../models/todo_type";
 
-const TodoItem: React.FC<{ todo: TodoType, changeToDoHandler: (id: string) => void, checked: boolean }> = (props) => {
+const TodoItem: React.FC<{ todo: TodoType, changeToDoHandler: (id: string) => void, checked: boolean, deleteToDoHandler: (id: string) => void }> = (props) => {
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         props.changeToDoHandler(e.target.value)
     }
 
+    const deleteHandler = (e: React.FormEvent<HTMLButtonElement>) => {
+        props.deleteToDoHandler(e.currentTarget.value);
+    }
+
     return (
         <li>
+            <button value={props.todo.id} onClick={deleteHandler}>
+                <DeleteIcon/>
+            </button>
             <input
                 type="checkbox"
                 id={props.todo.id}

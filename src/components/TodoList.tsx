97fd5f07dcb7a384f2b910f9d37dae7734549a36
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-03-12 17:55:42 +0530

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,9 +4,13 @@ import TodoType from "../models/todo_type";
 import TodoItem from "./TodoItem";
 import styles from '../styles/main.module.css'
 
-const TodoList: React.FC<{ items: TodoType[], removeHandler: (id: string) => void }> = (props) => {
+const TodoList: React.FC<{ items: TodoType[], removeHandler: (id: string) => void, deleteHandler: (id: string) => void }> = (props) => {
     const removeTodoItem = (id: string) => {
         props.removeHandler(id)
+    }
+
+    const deleteTodoItem = (id: string) => {
+        props.deleteHandler(id);
     }
 
     return (
@@ -14,7 +18,7 @@ const TodoList: React.FC<{ items: TodoType[], removeHandler: (id: string) => voi
             {props.items.length === 0 && <h3>Hurray! No To-Dos to Do</h3>}
             {props.items.length !== 0 && props.items
                 .map((todo) => 
-                    <TodoItem key={todo.id} todo={todo} changeToDoHandler={removeTodoItem} checked={false}/>
+                    <TodoItem key={todo.id} todo={todo} changeToDoHandler={removeTodoItem} checked={false} deleteToDoHandler={deleteTodoItem}/>
                 )
             }
         </ul>
